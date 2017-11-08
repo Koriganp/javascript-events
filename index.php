2c5a16fb5285483fadd636625d62ccb772eb4869
Korigan Payne <koriganp@gmail.com>
2017-11-08 15:45:23 -0700

    removed colorize from navbar

--- index.php
+++ index.php
@@ -21,12 +21,12 @@
 	</head>
 	<body class="colorize">
 		<header>
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-white colorize">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-white">
 				<a class="navbar-brand" href="#">JavaScript Events</a>
-				<button class="navbar-toggler colorize" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-				<div class="collapse navbar-collapse colorize" id="navbarSupportedContent">
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav ml-auto">
 						<li class="nav-item active">
 							<a class="nav-link" href="index.php">Reset <span class="sr-only">(current)</span></a>
